cf6f762e89d1dee49ed158338122ca76cc435fe2
Mariana <maribteixeira782_@outlook.com>
2026-07-09 21:14:36 -0300

    Remover Problem_Set_0 fora da pasta do curso CS50

--- Problem_Set_0/einstein.py
+++
@@ -1,9 +0,0 @@
-def main():
-    mass = int(input('m: '))
-    print('E: ', Joules(mass), sep="")
-
-def Joules(m):
-    E = m*300000000**2
-    return E
-
-main()
--- Problem_Set_0/indoor.py
+++
@@ -1,8 +0,0 @@
-def main():
-    nome = str(input("Digite seu nome: "))
-    print(tolowercase(nome))
-
-def tolowercase(x):
-    x = x.lower()
-    return x
-main()
